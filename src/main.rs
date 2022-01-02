b8f9b6fa90179bdbc626434180d4dc22f011671a
Justin Ely <justincely@gmail.com>
2022-01-01 22:13:36 -0500

    use cargo fmt to fix style errors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use rocket::response::NamedFile;
 use std::io;
 use std::thread;
 use std::time;
-use rocket::response::NamedFile;
-
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
@@ -15,22 +15,23 @@ fn hello(name: String, age: u8) -> String {
 
 #[get("/sleep/<time>")]
 fn sleep(time: u64) -> String {
-    let duration = time::Duration::from_millis(time*1000);
+    let duration = time::Duration::from_millis(time * 1000);
     let now = time::Instant::now();
 
     thread::sleep(duration);
 
     assert!(now.elapsed() >= duration);
     format!("Just napped for {} seconds!", time)
-
 }
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
-  NamedFile::open("static/index.html")
+    NamedFile::open("static/index.html")
 }
 
 /// Starts a new HTTP server.
 fn main() {
-    rocket::ignite().mount("/", routes![hello, sleep, index]).launch();
+    rocket::ignite()
+        .mount("/", routes![hello, sleep, index])
+        .launch();
 }
